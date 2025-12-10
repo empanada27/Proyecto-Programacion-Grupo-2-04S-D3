266ca2c5435f532d4ed794902bab1e39d8b974e4
angeldavid2808-maker <angeldavid2808@gmail.com>
2025-12-10 11:48:56 -0400

    Add files via upload
    Conteo números 1 al 10 
    Ángel cooz 31639110

---
+++ ángel cooz .cxx
@@ -0,0 +1,14 @@
+
+//angel cooz 31639110
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("Números del 1 al 10:\n");
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
